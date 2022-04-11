4372ebc1c9e7793433315d98493dff27d1ee6c5e
Erdinand Mujaj <36090421+erdinandmujaj@users.noreply.github.com>
2022-04-11 23:17:06 +0200

    edit read and write

--- Client.js
+++ Client.js
@@ -1,13 +1,58 @@
 const prompt = require("prompt-sync")({ sigint: true });
 var PORT = 33333;
+const clientIP = prompt("Sheno IP Addresen tuaj: ");
+var client = clientIP;
 const ipAddress = prompt("Sheno IP Addresen me te cilen doni te lidheni : ");
 var HOST = ipAddress;
+const fs = require ('fs');
+var path = 'mesazhi.txt';
+errorcode = "NUK KENI QASJE!"
+
 
 var dgram = require('dgram');
+// fs.chmod(path, 777, () => {
+//   console.log("\nReading the file contents");
+//   console.log(fs.readFileSync(path, 'utf8'));
+  
+//   console.log("\nTrying to write to file");
+//   try {
+//     fs.writeFileSync(path, "This file has now been edited.");
+//   }
+//   catch (e) {
+//     console.log("Error Code:",errorcode);
+//   }
+// });
+
+if (client == '192.168.0.25') {
+  console.log("\nGranting read and write access to user");
+  fs.chmod("mesazhi.txt", 0o600, () => {
+    
+    console.log("\nReading the file contents");
+    console.log(fs.readFileSync("mesazhi.txt", 'utf8'));
+    const ndryshimi = prompt("Ndryshoni file si read and write: ");
+
+    console.log("Ndryshimi i bere: ");
+    fs.writeFileSync('mesazhi.txt',ndryshimi);
+    console.log(ndryshimi);
+
+  });
+}
+else {
+  fs.chmod(path, 777, () => {
+    console.log("\nReading the file contents");
+    console.log(fs.readFileSync(path, 'utf8'));
+    
+    console.log("\nTrying to write to file");
+    try {
+      fs.writeFileSync(path, "This file has now been edited.");
+    }
+    catch (e) {
+      console.log("Error Code:",errorcode);
+    }});
+
 
 var client = dgram.createSocket('udp4');
 client.on('message', function (message, remote) {
-  // CAN I HANDLE THE RECIVED REPLY HERE????
   console.log(remote.address + ':' + remote.port +' - ' + message);
 
 });
@@ -21,4 +66,4 @@ client.send(message, 0, message.length, PORT, HOST, function(err, bytes) {
 
 
 
-
+}
--- Server.js
+++ Server.js
@@ -1,5 +1,52 @@
 var PORT = 33333;
 var HOST = '0.0.0.0';
+const fs = require ('fs');
+var path = 'mesazhi.txt';
+errorcode = "NUK KENI QASJE!"
+// var privilege = fs.chmod(path, 777, (err) => { 
+//   if (err) { 
+//   console.log(err); 
+//   }
+// });
+// fs.chmod(path, 777, () => {
+//   console.log("\nReading the file contents");
+//   console.log(fs.readFileSync(path, 'utf8'));
+  
+//   console.log("\nTrying to write to file");
+//   try {
+//     fs.writeFileSync(path, "This file has now been edited.");
+//   }
+//   catch (e) {
+//     console.log("Error Code:",errorcode);
+//   }
+// });
+
+// if (HOST = '192.168.0.28') {
+//   console.log("\nGranting read and write access to user");
+//   fs.chmod("example.txt", 0o600, () => {
+//     console.log("Trying to write to file");
+//     fs.writeFileSync('example.txt',' ');
+  
+//     console.log("\nReading the file contents");
+//     console.log(fs.readFileSync("example.txt", 'utf8'));
+//   });
+
+// }
+// else {
+// fs.chmod(path, 0o400, () => {
+//   console.log("\nReading the file contents");
+//   console.log(fs.readFileSync(path, 'utf8'));
+  
+//   console.log("\nTrying to write to file");
+//   try {
+//     fs.writeFileSync(path, "This file has now been edited.");
+//   }
+//   catch (e) {
+//     console.log("Error Code:",errorcode);
+//   }
+// });
+// }
+
 
 const { info } = require('console');
 var dgram = require('dgram');

2d141ec6561b8900a3f0ad651031435e145e35d0
Erdison Berisha <79603797+erdisonberisha@users.noreply.github.com>
2022-04-11 18:23:38 +0200

    serverResponse

--- Client.js
+++ Client.js
@@ -4,13 +4,21 @@ const ipAddress = prompt("Sheno IP Addresen me te cilen doni te lidheni : ");
 var HOST = ipAddress;
 
 var dgram = require('dgram');
+
+var client = dgram.createSocket('udp4');
+client.on('message', function (message, remote) {
+  // CAN I HANDLE THE RECIVED REPLY HERE????
+  console.log(remote.address + ':' + remote.port +' - ' + message);
+
+});
 const mesazhi = prompt("Send a message to server: ");
 var message = new Buffer.from(mesazhi);
 
-
-var client = dgram.createSocket('udp4');
 client.send(message, 0, message.length, PORT, HOST, function(err, bytes) {
   if (err) throw err;
   console.log('UDP message sent to ' + HOST +':'+ PORT);
-  client.close();
-});
+  });
+
+
+
+
--- Server.js
+++ Server.js
@@ -1,6 +1,7 @@
 var PORT = 33333;
 var HOST = '0.0.0.0';
 
+const { info } = require('console');
 var dgram = require('dgram');
 var server = dgram.createSocket('udp4');
 
@@ -8,9 +9,12 @@ server.on('listening', function() {
   var address = server.address();
  console.log('UDP Server listening on ' + address.address + ':' + address.port);
 });
-
 server.on('message', function(message, remote) {
  console.log(remote.address + ':' + remote.port +' - ' + message);
+ const reply = 'Mesazhi ka arritur me sukses ne server!';
+ server.send(reply, 0, reply.length, remote.port, remote.address, function(err, bytes) {
+  if (err) throw err;
+  });
 });
 
 server.bind(PORT, HOST);
